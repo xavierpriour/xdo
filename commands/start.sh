8f37a749b1a2622391654e3059b5ab1e2e071855
Xavier Priour <xavier.priour@bubblyware.com>
2017-05-05 23:31:15 +0200

    start: add logging

--- commands/start.sh
+++ commands/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # call from root of the project!
 
+echo "-- starting $* on $STG"
 docker-compose -f stages/$STG/docker-compose.yml up -d $*
 
 #if first run, launch meteor then wait for initial load do complete before launching nginx+vpn
