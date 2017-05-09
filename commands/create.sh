44b436c6ce6b87af787ea2e349793d2dfde89542
Xavier Priour <xavier.priour@bubblyware.com>
2017-05-09 19:26:34 +0200

    rename DOM_KEY to SSH_KEY for clearer naming

--- commands/create.sh
+++ commands/create.sh
@@ -15,7 +15,7 @@ if [ -z "$exists" ]; then
   echo "-- create $DOM_NAME"
   # copy the ssh key file (will need root password)
   # they should be called <pki> and <pki>.pub and be stored in the same (safe) local folder
-  cat $DOM_KEY.pub | ssh $SSH_USER@$SSH_HOST 'cat > .ssh/authorized_keys'
+  cat $SSH_KEY.pub | ssh $SSH_USER@$SSH_HOST 'cat > .ssh/authorized_keys'
 
   # engine-storage-driver is mandatory, otherwise crashes on kimsufi
   docker-machine -D create \
